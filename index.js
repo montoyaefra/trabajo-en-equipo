9b876d1345c32ac8223751a302f098bbba0ecb4d
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-04-11 14:29:11 -0600

    V1.0

---
+++ index.js
@@ -0,0 +1,16 @@
+fetch("./data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    showData(data);
+  });
+
+const showData = (data) => {
+  console.log(data);
+  let body = "";
+  for (let index = 0; index < data.length; index++) {
+    body += `<tr><td>${data[index].id}</td><td>${data[index].name}</td><td>${data[index].apellido}</td><td>${data[index].city}</td><td>${data[index].state}</td></tr>`;
+  }
+  document.querySelector(".tabla").innerHTML = body;
+};
